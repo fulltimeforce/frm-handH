ec6bc2407f6a739052cba71e44e675f363760814
Edward Alarco <edwarddac.56@gmail.com>
2025-08-07 13:37:07 -0500

    update: blog page

---
+++ page-template/blog.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    Template name: blog
+*/
+
+get_header();
+
+get_banner('Homepage / classic auctions / News and Insights', '', 'News and Insights');
+
+?>
+
+<section class="blog_section pblock160">
+    <div class="container">
+        <div class="blog_section-box">
+            <div class="blog_section-grid">
+
+                <div class="blog_article">
+                    <div class="blog_article-image">
+                        <img src="<?php echo IMG; ?>/3.jpg">
+                    </div>
+                    <div class="blog_article-content">
+                        <span>08/02/2023</span>
+                        <h3>Actor, Sir Michael Caine’s first car, heads to auction. £100,000 - £150,000 Being Offered with H&H Classics...</h3>
+                        <p>Iconic British actor, Sir Michael Caine CBE’s, first ever car, a 1968 Rolls-Royce Silver Shadow Drophead Coupe with a fascinating history, is being offered for sale at auction on 15 March at the Imperial War Museum, Duxford.</p>
+                        <a href="#">Read More >></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
